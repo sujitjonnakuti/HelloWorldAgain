b8e4ff239c244dabca4e122ac724fe567dedb4db
Sujit <sujitdotnet1@Gmail.com>
2018-06-08 14:13:21 -0700

    console app to get message from api

---
+++ HWAgain.Console/Program.cs
@@ -0,0 +1,25 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWAgain.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RestClient(ConfigurationManager.AppSettings["BusinessAPI"]);
+            var request = new RestRequest("api/Messages");
+
+            IRestResponse response = client.Execute(request);
+            var content = response.Content;
+
+            System.Console.WriteLine(content);
+            System.Console.ReadKey();
+        }
+    }
+}
