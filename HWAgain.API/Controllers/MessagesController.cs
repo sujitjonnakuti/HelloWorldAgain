b76790641214cdc018bf8563ccb0f52890097779
Sujit <sujitdotnet1@Gmail.com>
2018-06-08 13:50:34 -0700

    message api controller

---
+++ HWAgain.API/Controllers/MessagesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HWAgain.API.Controllers
+{
+    public class MessagesController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ HWAgain.API/Models/MessageModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HWAgain.API.Models
+{
+    /// <summary>
+    /// Model for the message
+    /// </summary>
+    public class MessageModel
+    {
+        /// <summary>
+        /// Tha actual content
+        /// </summary>
+        public string Content { get; set; }
+    }
+}
